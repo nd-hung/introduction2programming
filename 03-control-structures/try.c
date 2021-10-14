618a42c51c55cef8856268a7e197abec6fe3013f
Hung Nguyen <hungdhnt@gmail.com>
2021-10-14 22:57:03 +0700

    update

---
+++ 03-control-structures/try.c
@@ -0,0 +1,14 @@
+
+
+        if(<Biểu thức logic>)
+        { 
+            // Khối lệnh 1 được thực thi khi <Biểu thức logic> != 0
+            <Lệnh 1>;
+        }
+        else
+        { 
+            // Khối lệnh 2 được thực thi khi <Biểu thức logic> == 0
+            <Lệnh 2>;
+        }        
+
+
