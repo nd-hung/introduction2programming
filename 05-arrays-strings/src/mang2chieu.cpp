e9a16ed5afce264db1ec00852dcc1f7e4b4a6ef3
Hung Nguyen <hungdhnt@gmail.com>
2021-10-27 19:53:27 +0700

    clean up

--- 05-arrays-strings/src/mang2chieu.cpp
+++ 05-arrays-strings/src/mang2chieu.cpp
@@ -13,9 +13,10 @@ g) Sắp xếp ma trận sao cho trên mỗi dòng các phần tử tăng dần.
 
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX 20
+#define ARRAY_SIZE 20
+#define MAX_ELEMENT 50
 
-typedef int matran[MAX][MAX];
+typedef int matran[ARRAY_SIZE][ARRAY_SIZE];
 void TaoMT(matran a, int &m, int &n);   // a 
 void NhapMT(matran a, int &m, int &n);  // a
 void XuatMT(matran a, int m, int n);    // b
@@ -49,7 +50,7 @@ int main()
     XuatMT(a,m,n);
 }
 
-
+// Tạo ma trận mxn phần tử có giá trị ngẫu nhiên
 void TaoMT(matran a, int &m, int &n)
 {
     printf("Nhap so hang:");
@@ -60,11 +61,12 @@ void TaoMT(matran a, int &m, int &n)
     for(int i=0; i < m; i++)   
         for(int j=0; j < n; j++)
         {
-            // Tạo giá trị ngẫu nhiên <= 50 cho ma trận
-            a[i][j] = rand() % 50;
+            // Tạo giá trị ngẫu nhiên trong khoảng [0, MAX_ELEMENT) 
+            a[i][j] = rand() % MAX_ELEMENT;
         }
 }
 
+// Nhập ma trận từ bàn phím
 void NhapMT(matran a, int &m, int &n)
 {
     printf("Nhap so hang:");
@@ -105,7 +107,8 @@ int MaxMT(matran a, int m, int n)
     return max;
 }
 
-// Hàm LaSNT(int n) trả về 1 nếu n là số nguyên tố, ngược lại trả về 0
+/* Hàm LaSNT(int n) trả về 1 nếu n là số nguyên tố, ngược lại trả về 0
+*/
 int LaSNT(int n)
 {
     if(n < 2) return 0;
@@ -114,8 +117,8 @@ int LaSNT(int n)
     return 1;
 }
 
-/* Hàm LaMTSNT(int a, int m, int n) trả về 1 nếu toàn bộ phần tử là SNT
-ngược lại trả về 0
+/* Hàm LaMTSNT(int a, int m, int n) trả về 1 nếu toàn bộ phần tử là SNT,
+   ngược lại trả về 0
 */
 int LaMTSNT(matran a, int m, int n)
 {
@@ -123,18 +126,23 @@ int LaMTSNT(matran a, int m, int n)
         for(int j=0; j < n; j++)
             // Nếu có ít nhất 1 phần tử không là SNT thì trả về 0
             if(!LaSNT(a[i][j])) return 0;
-    // nếu không rơi vào trường hợp trên -> tất cả phần tử đều là SNT -> trả về 1
+    // nếu không xảy ra trường hợp trên -> tất cả phần tử đều là SNT -> trả về 1
     return 1;
 }
 
 /*
 Sắp xếp từng hàng tăng dần.
+Cách giải quyết:
+- Lần lượt xét từng hàng của ma trận.
+- Mỗi hàng của ma trận {a[i][0], a[i][1],..,a[i][n-1]} là mảng 1 chiều, 
+  tiến hành sắp xếp mảng 1 chiều.
 */
 void SapXepMT(matran a, int m, int n)
 {
     // Xét từng hàng của ma trận
     for(int i=0; i < m; i++)
     { // Sắp xếp các phần tử trên hàng i tăng dần
+      // giải thuật đổi chỗ trực tiếp
         for(int j=0; j < n-1; j++)
             for(int k=j+1; k < n; k++)
             if(a[i][j] > a[i][k]) 
