0e19f353f82bc5480070336eb11f4d3d3e332c59
Hung <hungdhnt@gmail.com>
2022-06-12 21:41:54 +0700

    update

--- 05-arrays-strings/src/bai3_2.cpp
+++ 05-arrays-strings/src/bai3_2.cpp
@@ -2,32 +2,33 @@
 Bai 3.2
 */
 
-#include<stdio.h>
+#include <stdio.h>
 
 long long GiaiThua(unsigned int n)
 {
-	long long s=1;
-	for(int i=2; i <= n; i++)
+	long long s = 1;
+	for (int i = 2; i <= n; i++)
 		s *= i;
 	return s;
 }
 
 long A(unsigned int k, unsigned int n)
 {
-	return GiaiThua(n) / GiaiThua(n-k);
+	return GiaiThua(n) / GiaiThua(n - k);
 }
 
 int main()
 {
-	int n, s=0;
-	
-	do{
+	int n, s = 0;
+
+	do
+	{
 		printf("Nhap so le n (3 <= n <=15):");
 		scanf("%d", &n);
-	}while(!(n % 2 != 0 && n >=3 && n <= 15));
-	
-	for(int i=1; i <=n; i+= 2)
-		s += A(i,n);
-	
+	} while (!(n % 2 != 0 && n >= 3 && n <= 15));
+
+	for (int i = 1; i <= n; i += 2)
+		s += A(i, n);
+
 	printf("S = %d\n", s);
 }
