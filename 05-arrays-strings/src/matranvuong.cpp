dd32f2e37e08d51297829bb61e386be128bf913d
Hung <hungdhnt@gmail.com>
2022-06-19 20:37:28 +0700

    update content

---
+++ 05-arrays-strings/src/matranvuong.cpp
@@ -0,0 +1,12 @@
+/*
+Viết chương trình xử lý ma trận vuông gồm các hàm sau:
+Hàm NhapMTV để nhập ma trận vuông bậc n (2 ≤ n ≤ 10).
+Hàm InMTV để in ra màn hình các phần tử của ma trận theo đúng định dạng.
+Hàm MaxTGT để tìm và trả về giá trị lớn nhất của các phần tử thuộc ma trận tam giác trên. Biết ma trận tam giác trên của ma trận vuông A_(n×n) là các phần tử {a_ij } thỏa mãn i ≤j.
+Hàm ThayThe để thay các phần tử thuộc đường chéo chính thành 1, các phần tử còn lại của ma trận thành 0.
+Hàm main lần lượt gọi các hàm trên để kiểm tra kết quả thực hiện.
+*/
+
+
+
+
